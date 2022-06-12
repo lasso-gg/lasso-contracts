29afbfde0311e60c4e5dc01612df631eec47ccfa
Nas <nas@Nass-MacBook-Pro.local>
2022-06-12 19:04:28 -0400

    temp create function

--- contracts/Lasso.sol
+++ contracts/Lasso.sol
@@ -11,6 +11,10 @@ import { CFAv1Library } from "@superfluid-finance/ethereum-contracts/contracts/a
 error FlowRateZero();
 error ServiceAlreadyActive(address owner);
 
+error EmptyHandle();
+
+error SubscriptionAlreadyExists(string title);
+
 contract Lasso {
     using CFAv1Library for CFAv1Library.InitData;
 
@@ -23,9 +27,18 @@ contract Lasso {
         string subscriptionType;
     }
 
+    struct Subscription {
+        bool active;
+        uint256 date;
+        uint256 flowRate;
+        string handle;
+        address owner;
+    }
+
     CFAv1Library.InitData public cfaV1;
 
     mapping(address => Service) public services;
+    mapping(address => Subscription[]) public subscriptions;
 
     constructor(ISuperfluid host) {
         cfaV1 = CFAv1Library.InitData(
@@ -43,12 +56,33 @@ contract Lasso {
     ) external returns (Service memory) {
         if (_flowRate == 0) revert FlowRateZero();
         if (services[msg.sender].active == true) revert ServiceAlreadyActive(msg.sender);
-
+        
         services[msg.sender] = Service(true, block.timestamp, 0, _flowRate, _title, _subscriptionType);
 
         emit ServiceCreated(msg.sender, block.timestamp, _flowRate, _title, _subscriptionType);
 
         return services[msg.sender];
+    }
+
+    function subscribe(
+        string calldata _handle,
+        address _owner,
+    ) external returns (Subscription memory) {
+        if (bytes(_handle).length == 0) revert EmptyHandle();
+
+        for (uint i=0; i < subscriptions[msg.sender].length; i++) {
+            if(subscriptions[msg.sender][i].owner == _owner && subscriptions[msg.sender][i].active){
+                revert SubscriptionAlreadyExists(services[_owner].title);
+            }
+        }
+
+        cfaV1.createFlow(receiver, token, flowRate);
+
+        subscriptions[msg.sender] = Subscription(true, block.timestamp, services[_owner].flowRate, _handle, _owner);
+
+        emit SubscriptionCreated(msg.sender, block.timestamp, _flowRate, _title, _subscriptionType);
+
+        return subscriptions[msg.sender]
     }
 
     event ServiceCreated(
